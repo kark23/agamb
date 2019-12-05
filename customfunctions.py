5daa9bed729672bb0e92b41576a778bd8855856e
kark23 <51769121+kark23@users.noreply.github.com>
2019-12-04 20:00:00 -0500

    yo

--- customfunctions.py
+++ customfunctions.py
@@ -4,16 +4,32 @@ def oddtoprob(sign,val):
     ref={'-': val,'+': 100}
     return ref[sign]/(100+val)
 
-def distri(odds):
+def build_dist(odds):
     prts=[]
     for odd in odds:
         prts+=odd[:2]
     prts=sorted(list(set(prts)))
-    #mainmatrix=matrix(len(odds),len(prts)-1)
-    for i in len(odds):
+    mainmat=np.zeros((len(odds),len(prts)-1))
+    cond=np.zeros(len(odds))
+    print(prts)
+    for i in range(len(odds)):
         ind1,ind2=(prts.index(odds[i][0]),prts.index(odds[i][1]))
-        inds=np.linspace(ind1,ind2,1+abs(ind1-ind2))
+        inds=np.linspace(ind1,ind2-1,ind2-ind1)
         for ind in inds:
-            #mainmatrix[i][int(ind)]=1
-        #cond[i]=odds[i][2]
-        #solve matrix
+            mainmat[i,int(ind)]=1
+        cond[i]=odds[i][2]
+    approx=np.matmul(np.linalg.inv(mainmat),cond)
+    print(approx)
+    def int_dist(lo,hi):
+        prob=0.
+        strt=False
+        for i in range(len(prts)):
+            if strt:
+                prob+=approx[i]
+            if lo>=prts[i] and lo<=prts[i+1]:
+                prob+=approx[i]*(prts[i+1]-lo)/(prts[i+1]-prts[i])
+                strt=True
+            if hi>=prts[i] and hi<=prts[i+1]:
+                prob-=approx[i]*(prts[i+1]-hi)/(prts[i+1]-prts[i])
+                return prob
+    return int_dist
